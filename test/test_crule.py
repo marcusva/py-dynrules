780c5ad8dbfd245204aca0525648228dd9432fb0
Marcus von Appen <marcus@sysfault.org>
2008-11-21 00:22:54 +0100

    Added unittests for CRuleSet, CRule, CLearnSystem classes.

---
+++ test/test_crule.py
@@ -0,0 +1,41 @@
+import unittest
+from dynrules import CRule
+
+class CRuleTest (unittest.TestCase):
+    def test_create (self):
+        rule = CRule (1)
+        self.assert_ (rule.id == 1)
+        self.assert_ (rule.weight == 0)
+        self.assert_ (rule.code == None)
+
+    def test_weight (self):
+        rule = CRule (1)
+        self.assert_ (rule.weight == 0)
+        rule.weight = 100
+        self.assert_ (rule.weight == 100.)
+
+        def setw ():
+            rule.weight = -100
+        self.assertRaises (ValueError, setw)
+
+    def test_used (self):
+        rule = CRule (1)
+        rule.used = 1
+        self.assert_ (rule.used == True)
+        rule.used = 0
+        self.assert_ (rule.used == False)
+        rule.used = not True
+        self.assert_ (rule.used == False)
+        rule.used = not False
+        self.assert_ (rule.used == True)
+
+    def test_code (self):
+        rule = CRule (1)
+        self.assert_ (rule.code == None)
+        rule.code = "Arbitrary string"
+        self.assert_ (rule.code == "Arbitrary string")
+        rule.code = buffer("Arbitrary string")
+        self.assert_ (isinstance (rule.code, buffer))
+
+if __name__ == "__main__":
+    unittest.main ()
---
+++ test/test_cruleset.py
@@ -0,0 +1,107 @@
+import unittest
+from dynrules import CRule, CRuleSet
+
+class CRuleSetTest (unittest.TestCase):
+    def test_create (self):
+        ruleset = CRuleSet (0, 20)
+        self.assert_ (ruleset.minweight == 0)
+        self.assert_ (ruleset.maxweight == 20)
+        self.assert_ (ruleset.weight == 0)
+        self.assert_ (ruleset.rules == [])
+
+    def test_minweight (self):
+        ruleset = CRuleSet (0, 20)
+        self.assert_ (ruleset.minweight == 0)
+        ruleset.minweight = 20
+        self.assert_ (ruleset.minweight == 20)
+
+        def setm (x):
+            ruleset.minweight = x
+        self.assertRaises (ValueError, setm, -1)
+        self.assertRaises (ValueError, setm, 50)
+
+    def test_maxweight (self):
+        ruleset = CRuleSet (10, 20)
+        self.assert_ (ruleset.maxweight == 20)
+        ruleset.maxweight = 10
+        self.assert_ (ruleset.maxweight == 10)
+
+        def setm (x):
+            ruleset.maxweight = x
+        self.assertRaises (ValueError, setm, -1)
+        self.assertRaises (ValueError, setm, 8)
+
+    def test_rulesadd (self):
+        ruleset = CRuleSet (10, 20)
+        
+        self.assertRaises (TypeError, ruleset.add, None)
+        self.assertRaises (TypeError, ruleset.add, 10)
+
+        ruleset.add (CRule (1))
+        ruleset.add (CRule (1))
+        ruleset.add (CRule (1))
+        self.assert_ (len (ruleset.rules) == 1)
+        self.assert_ (ruleset.weight == 10)
+
+        ruleset.add (CRule (2))
+        ruleset.add (CRule (3))
+        self.assert_ (len (ruleset.rules) == 3)
+        self.assert_ (ruleset.weight == 30)
+
+        ruleset.clear ()
+        self.assert_ (len (ruleset.rules) == 0)
+        self.assert_ (ruleset.weight == 0)
+
+    def test_rulesremove (self):
+        ruleset = CRuleSet (10, 20)
+        
+        self.assertRaises (TypeError, ruleset.add, None)
+        self.assertRaises (TypeError, ruleset.add, 10)
+
+        for i in xrange (10):
+            ruleset.add (CRule (i))
+        self.assert_ (len (ruleset.rules) == 10)
+        self.assert_ (ruleset.weight == 100)
+        rules = ruleset.rules
+
+        ruleset.remove (rules[3])
+        self.assert_ (len (ruleset.rules) == 9)
+        self.assert_ (ruleset.weight == 90)
+
+        self.assertRaises (ValueError, ruleset.remove, CRule (7))
+        self.assert_ (len (ruleset.rules) == 9)
+        self.assert_ (ruleset.weight == 90)
+
+        self.assertRaises (ValueError, ruleset.remove, rules[3])
+        ruleset.remove (rules[2])
+        self.assert_ (len (ruleset.rules) == 8)
+        self.assert_ (ruleset.weight == 80)
+
+        ruleset.clear ()
+        self.assert_ (len (ruleset.rules) == 0)
+        self.assert_ (ruleset.weight == 0)
+
+    def test_updateweights (self):
+        ruleset = CRuleSet (10, 20)
+
+        rule = None
+        for i in xrange (10):
+            rule = CRule (i)
+            rule.weight = 15
+            ruleset.add (rule)
+
+        ruleset.rules[3].used = True
+        ruleset.rules[7].used = True
+
+        ruleset.calculate_adjustment = lambda x: 3
+        ruleset.distribute_remainder = lambda x: None
+
+        ruleset.update_weights (None)
+        for x, rule in enumerate (ruleset.rules):
+            if x == 3 or x == 7:
+                self.assert_ (rule.weight > 15)
+            else:
+                self.assert_ (rule.weight < 15)
+
+if __name__ == "__main__":
+    unittest.main ()
